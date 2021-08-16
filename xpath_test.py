0cad07ca1312de270fec964a88c7d1d546301341
bbttboy <syryangliu@hotmail.com>
2021-08-16 16:19:31 +0800

    Update xpath_test.py

--- xpath_test.py
+++ xpath_test.py
@@ -7,13 +7,19 @@ html = """
     <price>1.23</price>
     <nick>臭豆腐</nick>
     <author>
-    <nick id="10086">周大强</nick>
-    <nick id="10010">周芷若</nick>
-    <nick class="joy">周杰伦</nick>
-    <nick class="jolin">蔡依林</nick>
-    <div>
-        <nick>热热热热1</nick>
-    </div>
+        <nick id="10086">周大强</nick>
+        <nick id="10010">周芷若</nick>
+        <nick class="joy">周杰伦</nick>
+        <nick class="jolin">蔡依林</nick>
+        <div>
+            <nick>热热热热1</nick>
+        </div>
+        <div>
+            <nick>热热热热2</nick>
+            <div>
+                <nick>热热热热3</nick>
+            </div>
+        </div>
     </author>
     
     <partner>
@@ -22,3 +28,13 @@ html = """
     </partner>
 </book>
 """
+
+tree = etree.XML(html)
+# result = tree.xpath("/book/name")  # /表示层级关系，第一个/是根节点， 返回list列表
+# result = tree.xpath("/book/name/text()")  # text() 拿文本
+# result = tree.xpath("/book/author//nick/text()")  # // 后代
+# result = tree.xpath("/book/author/*/nick/text()")  # * 任意的节点，通配符
+result = tree.xpath("/book//nick/text()")
+
+# print(type(result))
+print(result)
